3256576c6523c5c25ff065c7a79c900f7031da2a
betomoedano <betomoedano@outlook.com>
2024-06-29 13:30:21 -0500

    follow me! @betomoedano 🚀

--- App.tsx
+++ App.tsx
@@ -1,20 +1,129 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  NavigationContainer,
+  createComponentForStaticNavigation,
+  createStaticNavigation,
+  StaticScreenProps,
+  StaticParamList,
+  useNavigation,
+  NavigatorScreenParams,
+  useRoute,
+  DarkTheme,
+} from "@react-navigation/native";
+import {
+  NativeStackNavigationProp,
+  createNativeStackNavigator,
+} from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { Button, Platform, StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends StaticParamList<typeof MyStack> {}
+  }
+}
+
+function App() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title="go to screen two"
+        onPress={() => navigation.navigate("ScreenTwo")}
+        // onPress={() => navigation.navigate("Settings")}
+      />
+      <Button
+        title="go home"
+        onPress={() =>
+          navigation.navigate("ScreenOne", {
+            screen: "Home",
+            params: { userId: "hello" },
+          })
+        }
+      />
+
+      {/* Could not make this work :(
+      <Button
+        title="Preload Screen"
+        onPress={() => navigation.preload("ScreenOne", { screen: "Settings" })}
+      /> */}
     </View>
+  );
+}
+
+const MyStack = createNativeStackNavigator({
+  screens: {
+    ScreenOne: {
+      screen: MyTabs,
+      if: () => true, // This tells React Navigation to show specific screens based on the signed in status. When the signed in status changes, React Navigation will automatically show the appropriate screen.
+      options: {
+        headerTitle: "I'm a static screen!",
+        // headerShown: false,
+      },
+    },
+    ScreenTwo: {
+      screen: App,
+    },
+  },
+});
+
+// const MyStackComp = createComponentForStaticNavigation(MyStack, "Stack");
+const RootNavigation = createStaticNavigation(MyStack);
+
+const Tabs = createBottomTabNavigator();
+
+type MyTabsParamList = {
+  Home: { userId: string };
+  Profile: undefined;
+  Settings: undefined;
+};
+type MyTabsProps = StaticScreenProps<NavigatorScreenParams<MyTabsParamList>>;
+function MyTabs(_: MyTabsProps) {
+  return (
+    <Tabs.Navigator
+      screenOptions={{
+        tabBarPosition: Platform.select({ web: "left", ios: "top" }),
+      }}
+    >
+      <Tabs.Screen
+        name="Home"
+        component={App}
+        options={{ animation: "shift", headerShown: false }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        component={App}
+        options={{ animation: "shift" }}
+      />
+      <Tabs.Screen
+        name="Settings"
+        component={App}
+        options={{ animation: "shift" }}
+      />
+    </Tabs.Navigator>
+  );
+}
+
+export default function Navigation() {
+  return (
+    <RootNavigation />
+    //   <NavigationContainer
+    //     linking={{
+    //       prefixes: ["mytest://"],
+    //       enabled: true,
+    //     }}
+    //   >
+    //  </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,7 @@
+## YouTube Video
+
+Learn to create this app [youtube/@codewithbeto](https://www.youtube.com/@codewithbeto)
+
+## Courses and more resources
+
+For more info about this project, courses and resources visit [codewithbeto.dev](https://codewithbeto.dev)
